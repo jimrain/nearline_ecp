6ac35787512a7192e0cf254c6c9ceb3497b91d14
jimrain <jim@rainville.net>
2020-11-11 06:45:59 -0800

    Got GET form s3 working.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod aws;
+use aws::*;
+use chrono::DateTime;
+use chrono::Utc;
 use config::{Config, FileFormat};
 use fastly::http::{HeaderValue, Method, StatusCode};
 use fastly::request::CacheOverride;
@@ -10,9 +14,11 @@ use fastly::{Body, Error, Request, RequestExt, Response, ResponseExt};
 const CUSTOMER_ORIGIN: &str = "ShastaRain";
 
 /// The name of a second backend associated with this service.
-const NEARLINE_BACKEND: &str = "ShastaRain";
+const NEARLINE_BACKEND: &str = "backend_nlc";
 
 const LOGGING_ENDPOINT: &str = "nearline_syslog";
+
+const S3_DOMAIN: &str = "fsly-nlc-sfc.s3.us-west-002.backblazeb2.com";
 
 /// The entry point for your application.
 ///
@@ -26,28 +32,53 @@ fn main(mut req: Request<Body>) -> Result<impl ResponseExt, Error> {
     logging_init();
 
     log::debug!("URI: {:?}", req.uri());
+
+    let right_now = Utc::now();
+    let url = format!("{}", req.uri().path());
+    let headers = req.headers_mut();
+
+    headers.insert(
+        "Host",
+        HeaderValue::from_static("fsly-nlc-sfc.s3.us-west-002.backblazeb2.com"),
+    );
+
+    let auth_header = aws_v4_auth("s3", S3_DOMAIN, "", "GET", url.as_str(), right_now);
+    headers.insert("Authorization", auth_header.parse()?);
+
+    let x_amz_date = right_now.format("%Y%m%dT%H%M%SZ").to_string();
+    headers.insert("x-amz-date", x_amz_date.parse()?);
+    headers.insert("x-amz-content-sha256", empty_payload_hash().parse()?);
+
+    /*
+
+
+    .header("Host", HeaderValue::from_static(S3_DOMAIN))
+        .header("Authorization", aws_v4_auth("s3", S3_DOMAIN, "", "PUT", upload_url, now))
+        .header("x-amz-date", x_amz_date)
+        .header("x-amz-content-sha256", unsigned_payload_hash())
+     */
     let mut beresp = req.send(NEARLINE_BACKEND)?;
-
-    if beresp.status() == StatusCode::FORBIDDEN {
-        let mut new_req = beresp.fastly_metadata().unwrap().sent_req().unwrap();
-        beresp = Request::builder()
-            .method(new_req.method())
-            .uri(new_req.uri())
-            .body(())
-            .unwrap()
-            .send(CUSTOMER_ORIGIN)?;
-
-        if beresp.status() == StatusCode::OK {
-            let b = beresp.fastly_metadata().unwrap().sent_req().unwrap().uri().into_parts();
-            Request::builder()
-                .method(Method::PUT)
-                // .uri(new_req.uri())
+    /*
+        if beresp.status() == StatusCode::FORBIDDEN {
+            let mut new_req = beresp.fastly_metadata().unwrap().sent_req().unwrap();
+            beresp = Request::builder()
+                .method(new_req.method())
+                .uri(new_req.uri())
                 .body(())
                 .unwrap()
-                .send_async(NEARLINE_BACKEND)?;
-        }
-    }
+                .send(CUSTOMER_ORIGIN)?;
 
+            if beresp.status() == StatusCode::OK {
+                // let b = beresp.fastly_metadata().unwrap().sent_req().unwrap().uri().into_parts();
+                Request::builder()
+                    .method(Method::PUT)
+                    // .uri(new_req.uri())
+                    .body(())
+                    .unwrap()
+                    .send_async(NEARLINE_BACKEND)?;
+            }
+        }
+    */
     Ok(beresp)
 }
 
